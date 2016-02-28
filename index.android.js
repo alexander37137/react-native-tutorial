30ae00304c81e71b10ff410ea69bcde36ba28d93
Alexander Andreev <alexandr.andreev@mail.ru>
2016-02-28 13:12:57 +0300

    complete tutorial

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, { AppRegistry, Component, StyleSheet, Text, View, Image } from 'react-native';
+import React, { AppRegistry, Component, StyleSheet, Text, View, Image, ListView } from 'react-native';
 
 const MOCKED_MOVIES_DATA = [
     {
@@ -14,19 +14,70 @@ const MOCKED_MOVIES_DATA = [
         }
     }
 ]
+const REQUEST_URL = 'https:/raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 class ReactProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false,
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+                    loaded: true,
+                });
+            })
+            .done();
+    }
+
     render() {
-        const [movie, ...rest] = MOCKED_MOVIES_DATA;
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+
+        return (
+            <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderMovie}
+            style={styles.listView}
+            />
+            );
+    }
+
+    renderLoadingView() {
         return (
             <View style={styles.container}>
-              <Text>{movie.title}</Text>
-              <Text>{movie.year}</Text>
+                <Text>
+                    Loading movies...
+                </Text>
+            </View> )
+    }
+
+    renderMovie(movie) {
+        return (
+            <View style={styles.container}>
               <Image
-                style={styles.thumbnail}
-                source={{
-                    uri: movie.posters.thumbnail
-                }} />
+            style={styles.thumbnail}
+            source={{
+                uri: movie.posters.thumbnail
+            }} />
+                <View style={styles.rightContainer}>
+                  <Text style={styles.title}>{movie.title}</Text>
+                  <Text style={styles.year}>{movie.year}</Text>
+                </View>
             </View>
             );
     }
@@ -35,6 +86,7 @@ class ReactProject extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
@@ -42,7 +94,22 @@ const styles = StyleSheet.create({
     thumbnail: {
         width: 53,
         height: 81
-    }
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center'
+    },
+    year: {
+        textAlign: 'center'
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+    },
 });
 
 AppRegistry.registerComponent('ReactProject', () => ReactProject);
